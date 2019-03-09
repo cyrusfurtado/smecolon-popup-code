b8c36133ea7ef6b5577587dbfe3877b95e188eda
Cyrus Furtado <cyrus_furtado@persistent.co.in>
2019-03-09 13:30:04 +0530

    style changes

--- src/app/routed-components/emails/emails.component.ts
+++ src/app/routed-components/emails/emails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppServiceService } from '../../app-service.service';
 import { LimitTextPipe } from '../../pipes/limit-text.pipe';
 import * as moment from 'moment';
@@ -26,14 +27,14 @@ export interface Emails {
 export class EmailsComponent {
   messages = ['Verifying', 'Initializing', 'were done'];
   modalMessage: string;
-  isAnalyzed: boolean = false;
-  showPage2: boolean = false;
   naText = '--';
   emails: Array<Emails> = [];
   selectedEmail: Emails;
 
-  constructor(private app: AppServiceService, private limitText: LimitTextPipe) {
+  constructor(private app: AppServiceService, private limitText: LimitTextPipe, private route: Router) {
     console.log('moment', moment(new Date()).format('DD MM YYYY'));
+    this.app.loaderEvent.emit({hideloader: false});
+
     const getData = (data) => {
       this.emails = data ? data.map((email) => {
         var desc: string;
@@ -54,7 +55,7 @@ export class EmailsComponent {
 
         return Object.assign({}, email, ext);
       }) : [];
-      this.app.loaderEvent.emit(true);
+      this.app.loaderEvent.emit({hideloader: true});
     }
     this.app.getEmails().subscribe(getData);
     // getData([{"mailId":"14","createdAt":"2019-03-08T09:17:17.577Z","subject":"An email","date":"Thu, 4 Mar 2010 15:35:32 -0800","from":"george@foo.com","to":"paul@goo.com","sender":"paul@goo.com","body":"<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna?</p><p>Aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse.</p><p>Cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est.</p><p>Laborum sed ut perspiciatis unde omnis iste natus error sit. Voluptatem accusantium doloremque laudantium totam.</p><p>Rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>"},{"mailId":"15","createdAt":"2019-03-08T09:17:18.407Z","subject":"An email","date":"Thu, 4 Mar 2010 15:35:32 -0800","from":"george@foo.com","to":"paul@goo.com","sender":"paul@goo.com","body":"<p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna?</p><p>Aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse.</p><p>Cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est.</p><p>Laborum sed ut perspiciatis unde omnis iste natus error sit. Voluptatem accusantium doloremque laudantium totam.</p><p>Rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>"}]);
@@ -66,15 +67,16 @@ export class EmailsComponent {
   }
 
   analyzeMail() {
-    this.app.loaderEvent.emit(false);
+    this.app.loaderEvent.emit({hideloader: false});
     let count = 0;
     const callback = () => {
       console.log('ffsfasf', count);
       this.modalMessage = this.messages[count];
+      this.app.loaderEvent.emit({hideloader: undefined, message: this.modalMessage});
       if (count >= this.messages.length) {
         this.modalMessage = '';
-        this.app.loaderEvent.emit(true);
-        this.showPage2 = this.isAnalyzed = true;
+        this.app.loaderEvent.emit({hideloader: true});
+        this.route.navigateByUrl('/resumelist');
         clearInterval(timer);
       }
       count++;
--- src/app/routed-components/resumelist/resumelist.component.ts
+++ src/app/routed-components/resumelist/resumelist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from '../../app-service.service';
 
 @Component({
   selector: 'app-resumelist',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumelistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private app: AppServiceService) { }
 
   ngOnInit() {
+    this.app.loaderEvent.emit({hideloader: true});
   }
 
 }
