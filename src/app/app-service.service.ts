e7ea28aca8ab79dfa23ae9a72de53bf92f10a9a9
Cyrus Furtado <cyrus_furtado@persistent.co.in>
2019-03-09 13:30:04 +0530

    Style changes

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -2,15 +2,20 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface LoaderEvent {
+  hideloader: boolean;
+  message?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AppServiceService {
 
-  loaderEvent = new EventEmitter<any>();
+  loaderEvent = new EventEmitter<LoaderEvent>();
   constructor(private http: HttpClient) { }
 
   getEmails(): Observable<any> {
-    return this.http.get('http://10.244.25.148:4567/mails');
+    return this.http.get('http://10.244.25.36:4567/mails');
   }
 }
