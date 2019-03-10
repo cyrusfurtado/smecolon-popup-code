439302d6f6b1df37f93437779c6a93ce53542ed4
Cyrus Furtado <cyrus_furtado@persistent.co.in>
2019-03-10 05:50:07 +0530

    mock stop polling

--- src/app/routed-components/emails/emails.component.ts
+++ src/app/routed-components/emails/emails.component.ts
@@ -5,6 +5,7 @@ import { LimitTextPipe } from '../../pipes/limit-text.pipe';
 import { RemoveSpecialCharsPipe } from '../../pipes/remove-special-chars.pipe';
 import { TitlecasePipe } from '../../pipes/titlecase.pipe';
 import * as moment from 'moment';
+import { Subscription } from 'rxjs';
 
 export interface Emails {
   mailId: string;
@@ -127,14 +128,14 @@ export class EmailsComponent {
     this.app.loaderEvent.emit({hideloader: true});
   }
 
+  analyzeSub: Subscription;
   callback() {
     this.app.loaderEvent.emit({hideloader: false});
     this.modalMessage = this.messages[this.pollCount] ? this.messages[this.pollCount] : this.modalMessage;
     this.app.loaderEvent.emit({hideloader: undefined, message: this.modalMessage});
-    this.app.analyzePoll().subscribe((data: any) => {
+    this.analyzeSub = this.app.analyzePoll().subscribe((data: any) => {
         if(data.flag) {
-          this.stoploading();
-          this.app.noteEvent.emit({show: true, message: 'IND42942093572057209'});
+          this.stopPolling();
         } else {
           this.callback();
         }
@@ -142,9 +143,19 @@ export class EmailsComponent {
     this.pollCount++;
   }
 
+  stopPolling() {
+    this.stoploading();
+    this.app.noteEvent.emit({show: true, message: 'IND42942093572057209'});
+    this.analyzeSub.unsubscribe();
+  }
+
   acceptModal() {
     this.hideModal();
     this.callback();
+
+    setTimeout(() => {
+      this.stopPolling();
+    }, 30000);
   }
 
   hideModal(){
